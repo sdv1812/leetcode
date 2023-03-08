862e7d5bd5a12138a53b9a9d488502cb8730d5dd
sanskar <sanskar.verma90@gmail.com>
2023-03-08 17:34:36 +1100

    reverse int

--- app/src/main/kotlin/com/sanskar/leet/code/strings/easy/ReverseInteger.kt
+++ app/src/main/kotlin/com/sanskar/leet/code/strings/easy/ReverseInteger.kt
@@ -1,13 +1,26 @@
 package com.sanskar.leet.code.strings.easy
 
+import kotlin.math.pow
+
 class ReverseInteger {
     fun reverse(x: Int): Int {
-
+        var reversedNum = 0
+        var num = x
+        while (num != 0) {
+            val lastDigit = num % 10
+            if (reversedNum > Integer.MAX_VALUE/10 || (reversedNum == Integer.MAX_VALUE / 10 && lastDigit > 7)) return 0
+            if (reversedNum < Integer.MIN_VALUE/10 || (reversedNum == Integer.MIN_VALUE / 10 && lastDigit < -8)) return 0
+            reversedNum = reversedNum * 10 + lastDigit
+            num /= 10
+        }
+        return reversedNum
     }
 }
 
 fun main() {
-    val x = 123
+    val x = -321
     val r = ReverseInteger()
+    val y = 2.0.pow(10).toInt()
     println(r.reverse(x))
+    println(r.reverse(y))
 }
