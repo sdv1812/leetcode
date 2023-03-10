ad227740c6654197920e874c6fe09c6094b83ff1
sanskar <sanskar.verma90@gmail.com>
2023-03-10 15:56:22 +1100

    Remove Nth node

--- app/src/main/kotlin/com/sanskar/leet/code/linkedlist/easy/RemoveNthNode.kt
+++ app/src/main/kotlin/com/sanskar/leet/code/linkedlist/easy/RemoveNthNode.kt
@@ -4,9 +4,10 @@ class RemoveNthNode {
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         var i = 0
         var j = 0
-        var ptr1 = head
+        val dummy = ListNode(0, head)
+        var ptr1: ListNode? = dummy
         var ptr2 = head
-        while (ptr2?.next != null) {
+        while (ptr2!!.next != null) {
             if (j >= n - 1) {
                 i++
                 ptr1 = ptr1?.next
@@ -14,21 +15,32 @@ class RemoveNthNode {
             ptr2 = ptr2.next
             j++
         }
-        // delete ptr1
-        ptr1?.value = ptr1?.next?.value!!
-        ptr1.next = ptr1.next!!.next
-
-        return head
+        ptr1?.next = ptr1?.next?.next
+        return dummy.next
     }
 }
 
 fun main() {
     val r = RemoveNthNode()
-    val head = createLinkedList(intArrayOf(1, 2, 3, 4, 5))
-    printLinkedList(head)
 
     val head2 = createLinkedList2(intArrayOf(1, 2, 3, 4, 5, 6, 7))
     printLinkedList(head2)
-    val removedHead = r.removeNthFromEnd(head2, 2)
-    printLinkedList(removedHead)
+    val removedHead2 = r.removeNthFromEnd(head2, 2)
+    printLinkedList(removedHead2)
+
+    val head3 = createLinkedList2(intArrayOf(1))
+    printLinkedList(head3)
+    val removedHead3 = r.removeNthFromEnd(head3, 1)
+    printLinkedList(removedHead3)
+
+    val head4 = createLinkedList2(intArrayOf(1, 2))
+    printLinkedList(head4)
+    val removedHead4 = r.removeNthFromEnd(head4, 1)
+    printLinkedList(removedHead4)
+//
+    val head5 = createLinkedList2(intArrayOf(1, 2, 3, 4, 5, 6, 7))
+    printLinkedList(head5)
+    val removedHead5 = r.removeNthFromEnd(head5, 7)
+    printLinkedList(removedHead5)
+
 }
