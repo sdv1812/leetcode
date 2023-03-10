2c9371602b5c02196a8ec337132713f9dea4b8c4
sanskar <sanskar.verma90@gmail.com>
2023-03-10 12:29:52 +1100

    Creating a linked list from an array: 2 ways

--- app/src/main/kotlin/com/sanskar/leet/code/linkedlist/easy/Util.kt
+++ app/src/main/kotlin/com/sanskar/leet/code/linkedlist/easy/Util.kt
@@ -8,3 +8,24 @@ fun printLinkedList(head: ListNode?) {
     }
     println()
 }
+
+fun createLinkedList(arr: IntArray): ListNode? {
+    val head = if (arr.isEmpty()) null else ListNode(arr[0])
+    head ?: return null
+    var ptr = head
+    for (i in 1 until arr.size) {
+        ptr?.next = ListNode(arr[i], null)
+        ptr = ptr?.next
+    }
+    return head
+}
+
+fun createLinkedList2(arr: IntArray): ListNode? {
+    var head : ListNode? = null
+    var nextNode: ListNode?
+    for (i in arr.size - 1 downTo 0) {
+        nextNode = head
+        head = ListNode(arr[i], nextNode)
+    }
+    return head
+}
